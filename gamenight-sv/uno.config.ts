1856b17393fadfdc6205da864e39a6f396911160
Abhay Gupta <ab18gu@gmail.com>
2025-10-01 10:56:41 -0700

    Change cover photo of myself w/ teddy

--- gamenight-sv/uno.config.ts
+++ gamenight-sv/uno.config.ts
@@ -1,16 +1,30 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
-  content: {
-    filesystem: ['**/*.{html,js,ts,svelte}']
-  },
-  rules: [
-    ['text-color', { color: '#71548a' }],
-    ['hyperlinks', { color: '#096b6c' }],
-    ['background', { color: '#bcdeff' }],
-    ['text', { color: '#71548a' }],
-    ['b', { color: '#1A5270' }],
-    ['strong', { color: '#1A5270' }]
-  ]
+    theme: {
+        colors: {
+            "blue-grey": "#1A5270", // class="text-very-cool"
+        },
+    },
+    rules: [
+        ['text-color', { color: '#71548a' }],
+        ['hyperlinks', { color: '#096b6c' }],
+        ['background', { color: '#bcdeff' }],
+        ['text', { color: '#71548a' }],
+        ['h1',  { color: 'blue-grey' }],
+        ['h2',  { color: 'blue-grey' }],
+    ],
+    preflights: [
+        {
+        getCSS: () => `
+            h1, h2, h3, h4, h5, h6{
+            }
+            body {
+            color: #71548a;
+            background-color: #bcdeff;
+            },
+        `,
+        },
+    ],
 })

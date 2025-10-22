3b1882687bddcabd07f96ea8205500d2777c5d9d
Abhay Gupta <ab18gu@gmail.com>
2025-10-21 20:26:40 -0700

    Get all colors set for sv

--- gamenight-sv/uno.config.ts
+++ gamenight-sv/uno.config.ts
@@ -1,30 +1,10 @@
 // uno.config.ts
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetUno } from "unocss";
 
 export default defineConfig({
-    theme: {
-        colors: {
-            "blue-grey": "#1A5270", // class="text-very-cool"
-        },
-    },
-    rules: [
-        ['text-color', { color: '#71548a' }],
-        ['hyperlinks', { color: '#096b6c' }],
-        ['background', { color: '#bcdeff' }],
-        ['text', { color: '#71548a' }],
-        ['h1',  { color: 'blue-grey' }],
-        ['h2',  { color: 'blue-grey' }],
-    ],
-    preflights: [
-        {
-        getCSS: () => `
-            h1, h2, h3, h4, h5, h6{
-            }
-            body {
-            color: #71548a;
-            background-color: #bcdeff;
-            },
-        `,
-        },
-    ],
-})
+  preflights: [], // use base.css instead
+  theme: {
+    colors: {},
+  },
+  rules: [],
+});
